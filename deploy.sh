3c6390a60a0a40a78ffacbf574fbb8f53b41893a
DioSptra <saputradio111@gmail.com>
2025-05-10 22:33:26 +0700

    lets go 8

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,15 @@
 
 # Install dependencies untuk Flask app
 echo "Installing dependencies..."
-cd /home/ubuntu/flask-login-app
+cd /home/ubuntu/login_rds
 pip3 install -r requirements.txt
 
 # Menghentikan proses Flask yang sudah berjalan sebelumnya
 echo "Stopping any previous Flask processes..."
 pkill -f app.py || true
+
+# Pastikan direktori tempat aplikasi Flask ada
+mkdir -p /home/ubuntu/login_rds
 
 # Jalankan Flask app di background
 echo "Starting Flask app..."
