b8a26d6c79e7f1693a74c28b8521feedd092b39c
DioSptra <saputradio111@gmail.com>
2025-05-10 22:50:17 +0700

    lets go 10

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,18 @@
 # Install dependencies untuk Flask app
 echo "Installing dependencies..."
 cd /home/ubuntu/login_rds
-echo "install flask"
-sudo apt update
-sudo apt install python3-pip
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # Menghentikan proses Flask yang sudah berjalan sebelumnya
 echo "Stopping any previous Flask processes..."
-pkill -f app.py || true
+pkill -f app.py || true  # Memastikan tidak ada Flask app yang sedang berjalan
 
 # Pastikan direktori tempat aplikasi Flask ada
 mkdir -p /home/ubuntu/login_rds
 
 # Jalankan Flask app di background
 echo "Starting Flask app..."
-nohup python3 app.py > out.log 2>&1 &
+nohup python3 /home/ubuntu/login_rds/app.py > out.log 2>&1 &  # Jalankan Flask di background dan redirect output ke out.log
 
 echo "Deployment selesai!"
